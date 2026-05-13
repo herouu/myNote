ae164931223fcd0cbb25c9344b90e3317b9af0a7
fuq <fuq@digitalchina.com.cn>
2026-05-13 23:33:50 +0800

    feat(scripts): 配置Vite开发与生产模式

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   root: './src',
-  mode: 'production',
   build: {
     outDir: '../dist',
     minify: false,
